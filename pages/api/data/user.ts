74d4adcd0b39ed26f80b0f9246404c640d827093
Wiynxr <mkx.dake@gmail.com>
2023-02-27 14:19:08 +0300

    Create custom API

---
+++ pages/api/data/user.ts
@@ -0,0 +1,40 @@
+export const user = {
+  position: {
+    position: [
+      { id: 1, title: "Frontend Developer" },
+      { id: 2, title: "Backend Developer" },
+    ],
+    title: "Я разрабатываю и кодирую на React, и мне нравится то, что я делаю.",
+  },
+  aboutme: {
+    title: "Привет, я Минкаил. Приятно познакомиться.",
+    text: "Начав свой путь frontend-разработчика 3 года назад, я выполнял удаленную работу на фрилансе, участвовал в командных проектах и сотрудничал с талантливыми людьми, разрабатывая приложения для бизнеса, так и для потребителей. Я спокойно уверен в себе, любопытен от природы и постоянно работаю над совершенствованием своих навыков, решая по несколько задач за раз.",
+  },
+  skills: [
+    {
+      id: 1,
+      frontend: {
+        id: 1,
+        icon: "http://localhost:3000/_next/image?url=%2Fcode.png&w=64&q=75",
+        position: {
+          position: "Frontend",
+          title:
+            "Мне нравится кодировать вещи с нуля, и мне нравится воплощать идеи в жизнь в браузере.",
+        },
+        language: [{ name: "JavaScript" }],
+        dev_tools: [
+          { id: 1, name: "React" },
+          { id: 2, name: "Next" },
+          { id: 3, name: "Svelte" },
+          { id: 4, name: "Redux Toolkit" },
+          { id: 5, name: "TypeScript" },
+          { id: 6, name: "Babel" },
+          { id: 7, name: "Webpack" },
+          { id: 8, name: "REST API" },
+        ],
+      },
+    },
+  ],
+  projects: [],
+  companies: [],
+};
