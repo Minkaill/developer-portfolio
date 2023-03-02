e048882683a9d1a46d58a4ccd81a1452f5259d42
Wiynxr <mkx.dake@gmail.com>
2023-03-02 15:17:59 +0300

    fix

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,17 +1,17 @@
-import Aboutme from "@/components/Aboutme";
-import Position from "@/components/Position";
+import Aboutme from "../components/Aboutme";
+import Position from "../components/Position";
 import styles from "../styles/Home.module.scss";
 import axios from "axios";
 import { animate, scroll } from "motion";
 import React from "react";
-import Stack from "@/components/Stack";
-import { IUser } from "@/types";
+import Stack from "../components/Stack";
+import { IUser } from "../types";
 import { GetStaticProps, InferGetStaticPropsType } from "next";
 import Companies from "../components/Companies";
-import Footer from "@/components/Footer";
+import Footer from "../components/Footer";
 
 export const getStaticProps: GetStaticProps = async () => {
-  const { data } = await axios.get(`${process.env.API_HOST}/user/`);
+  const { data } = await axios.get("http://localhost:3000/api/user/");
   const user: IUser[] = data;
 
   if (!data) {
